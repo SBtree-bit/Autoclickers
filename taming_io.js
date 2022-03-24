a4eedfb419dd857198a036f70f5321c730b5f487
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2022-03-24 14:13:18 -0700

    Update taming_io.js

--- taming_io.js
+++ taming_io.js
@@ -1,15 +1,10 @@
 window.tamingAuto = function() {
   var keyAuto = document.createElement("div")
   var element = `
-    <h1 class="control-description" style="width:140px;">
-      <span class="shadowed">Autoclicker</span>
-      &nbsp&nbsp&nbsp&nbsp
-      <div id="auto" class="controls-input popup-input" style="margin-left:0px;">
-        <div id="_auto" class="control-selection">
-          C
-        </div>
-      </div
-    </h1>`
+    <div>
+      <select name="clicks" id="clicks">
+			</select>
+    </div>`
   keyAuto.innerHTML = element.trim(); 
   document.querySelector(".key-category-box").appendChild(keyAuto.firstChild)
 }
